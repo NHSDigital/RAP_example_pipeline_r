28ea89ba256fabb1f76a8ff59e45f27464c2ec27
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-01-23 15:27:36 +0000

    added download file function

--- create_publication.r
+++ create_publication.r
@@ -1,3 +1,15 @@
 source("src/data_ingestion/import_data.r")
 
-sayHello()
+# Config params
+hes_data_url <- "https://s3.eu-west-2.amazonaws.com/files.digital.nhs.uk/assets/Services/Artificial+data/Artificial+HES+final/artificial_hes_ae_202302_v1_sample.zip"
+download_destination <- "data_in/artificial_hes_ae_202302_v1_sample.zip"
+  
+
+
+pipeline <- function() {
+    sayHello()
+    download_file(hes_data_url, download_destination)
+}
+
+pipeline()
+
--- src/data_ingestion/import_data.r
+++ src/data_ingestion/import_data.r
@@ -1,3 +1,7 @@
 sayHello <- function() {
    print('hellooooooo')
 }
+
+download_file <- function(url, destination) {
+   download.file(hes_data_url, download_destination)
+}
