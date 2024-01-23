a3f0404555bb64c1c58b60ab45e650949906f634
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-01-23 16:53:08 +0000

    added functions to download and unzip files

--- create_publication.r
+++ create_publication.r
@@ -1,15 +1,21 @@
 source("src/data_ingestion/import_data.r")
 
 # Config params
-hes_data_url <- "https://s3.eu-west-2.amazonaws.com/files.digital.nhs.uk/assets/Services/Artificial+data/Artificial+HES+final/artificial_hes_ae_202302_v1_sample.zip"
-download_destination <- "data_in/artificial_hes_ae_202302_v1_sample.zip"
-  
+hes_data_url <- "https://s3.eu-west-2.amazonaws.com/files.digital.nhs.uk/assets/Services/Artificial+data/Artificial+HES+final/"
+hes_data_file_name <- "artificial_hes_ae_202302_v1_sample.zip"
+download_destination <- "data_in/"
 
 
 pipeline <- function() {
-    sayHello()
-    download_file(hes_data_url, download_destination)
+  sayHello()
+  download_file(
+    paste(hes_data_url, hes_data_file_name, sep = ""),
+    paste(download_destination, hes_data_file_name, sep = "")
+  )
+  unzip_file(
+    paste(download_destination, hes_data_file_name, sep = ""),
+    download_destination
+  )
 }
 
 pipeline()
-
--- src/data_ingestion/import_data.r
+++ src/data_ingestion/import_data.r
@@ -5,3 +5,7 @@ sayHello <- function() {
 download_file <- function(url, destination) {
    download.file(url, destination)
 }
+
+unzip_file <- function(zip_file_path, output_path) {
+   unzip(zip_file_path, exdir=output_path)
+}
