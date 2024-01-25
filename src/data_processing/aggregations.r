ed6887a4436e59fc47f538e797e47efefa84d05e
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-01-25 16:24:08 +0000

    moved aggregation to a separate function

---
+++ src/data_processing/aggregations.r
@@ -0,0 +1,14 @@
+get_distinct_count_of_col <- function(df_input, col_name) {
+    #' @title function get_distinct_count_of_col
+    #' 
+    #' @description Returns the distint count of a single column
+    #' in a dataframe.
+    #' 
+    #' @param df_input dataframe: The dataframe containing the column you want to count
+    #' @param col_name string: The name of the column you want to count
+    #' 
+    #' @section Returns: dataframe: df with one column and one row containing the distinct count
+    
+    df_aggregated <- df_input %>% summarise(EPIKEY = n_distinct(df_input[[col_name]]))
+    return(df_aggregated)
+}
