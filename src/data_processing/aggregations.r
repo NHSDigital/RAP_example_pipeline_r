07336a3046e8f6440cbda25ca9f075c35db374bc
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-01-25 17:18:15 +0000

    removed unused code from get_distinct_count_of_col

--- src/data_processing/aggregations.r
+++ src/data_processing/aggregations.r
@@ -8,8 +8,6 @@ get_distinct_count_of_col <- function(df_input, col_name) {
     #' @param col_name string: The name of the column you want to count
     #' 
     #' @section Returns: dataframe: df with one column and one row containing the distinct count
-    
-    new_column <- enquo(col_name)
 
     df_aggregated <- df_input %>% summarise({{col_name}} := n_distinct(df_input[[col_name]]))
     return(df_aggregated)
