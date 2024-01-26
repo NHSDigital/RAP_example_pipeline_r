7a695a4fa6d7f663a75b3ee2bf6fd2eace1479f6
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-01-26 11:56:23 +0000

    moved params to config.yml

--- create_publication.r
+++ create_publication.r
@@ -6,35 +6,27 @@ library(dplyr)
 source("src/data_ingestion/import_data.r")
 source("src/data_processing/aggregations.r")
 
-docstring(download_file_if_not_exists)
-
-# Config params
-hes_data_url <- "https://s3.eu-west-2.amazonaws.com/files.digital.nhs.uk/assets/Services/Artificial+data/Artificial+HES+final/"
-hes_data_file_name <- "artificial_hes_ae_202302_v1_sample.zip"
-download_destination <- "data_in/"
-downloaded_hes_folder <- "data_in/artificial_hes_ae_202302_v1_sample/"
-downloaded_hes_file <- "artificial_hes_ae_2122.csv"
-output_path <- "data_out/"
+config <- config::get()
 
 pipeline <- function() {
 
   download_file_if_not_exists(
-    paste(hes_data_url, hes_data_file_name, sep = ""),
-    paste(download_destination, hes_data_file_name, sep = "")
+    paste(config$hes_data_url, config$hes_data_file_name, sep = ""),
+    paste(config$download_destination, config$hes_data_file_name, sep = "")
   )
   unzip(
-    paste(download_destination, hes_data_file_name, sep = ""),
-    exdir = download_destination
+    paste(config$download_destination, config$hes_data_file_name, sep = ""),
+    exdir = config$download_destination
   )
   df_hes <- read.csv(
-    paste(downloaded_hes_folder, downloaded_hes_file, sep = "")
+    paste(config$downloaded_hes_folder, config$downloaded_hes_file, sep = "")
   )
 
   df_hes_england_episode_count <- get_distinct_count_of_col(df_hes, "EPIKEY")
 
   write.csv(
     df_hes_england_episode_count,
-    file = paste(output_path, "hes_england_episode_count", sep = ""),
+    file = paste(config$output_path, "hes_england_episode_count", sep = ""),
     row.names = FALSE,
     quote = FALSE
   )
